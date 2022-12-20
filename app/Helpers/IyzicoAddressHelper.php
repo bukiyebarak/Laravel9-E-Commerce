5b4f32708627cdcf999160ed5b439367ca17c3a3
bukiyebarak <bukiyebarak810@gmail.com>
2022-12-20 09:21:13 +0300

    Düzenleme

--- app/Helpers/IyzicoAddressHelper.php
+++ app/Helpers/IyzicoAddressHelper.php
@@ -10,7 +10,7 @@ class IyzicoAddressHelper
     public static function getAddress(Order $order): Address
     {
         $shippingAddress = new Address();
-        $shippingAddress->setContactName($order->name . '' . $order->surname);
+        $shippingAddress->setContactName($order->name . ' ' . $order->surname);
         $shippingAddress->setCity($order->city);
         $shippingAddress->setCountry("Türkiye");
         $shippingAddress->setAddress($order->address);
